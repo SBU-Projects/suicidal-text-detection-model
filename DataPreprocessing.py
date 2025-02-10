8cc4b8e003521e5dd2f2e55d224789eb335d210f
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-02-10 12:36:54 +0330

    text_preprocessing

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -66,3 +66,50 @@ class DP:
     def fix_lengthening(self, text):
         pattern = re.compile(r"(.)\1{2,}")
         return pattern.sub(r"\1\1", text)
+
+    def text_preprocessing(self, text, accented_chars=True, contractions=True, convert_num=True,
+                           extra_whitespace=True, lemmatization=True, lowercase=True,
+                           url=True, symbols_digits=True, special_chars=True,
+                           stop_words=True, lengthening=True, spelling=True):
+        """preprocess text with default option set to true for all steps"""
+        if accented_chars == True:  # remove accented characters
+            text = self.remove_accented_chars(text)
+        if contractions == True:  # expand contractions
+            text = contract.fix(text)
+        if lowercase == True:  # convert all characters to lowercase
+            text = text.lower()
+        if url == True:  # remove URLs before removing symbols
+            text = self.remove_url(text)
+        if symbols_digits == True:  # remove symbols and digits
+            text = self.remove_symbols_digits(text)
+        if special_chars == True:  # remove special characters
+            text = self.remove_special(text)
+        if extra_whitespace == True:  # remove extra whitespaces
+            text = self.remove_whitespace(text)
+        if lengthening == True:  # fix word lengthening
+            text = self.fix_lengthening(text)
+        if spelling == True:  # fix spelling
+            text = self.fix_spelling(text)
+
+        doc = self.nlp(text)  # tokenise text
+
+        clean_text = []
+
+        # return text
+
+        for token in doc:
+            flag = True
+            edit = token.text
+            # remove stop words
+            if stop_words == True and token.is_stop and token.pos_ != 'NUM':
+                flag = False
+            # exclude number words
+            if convert_num == True and token.pos_ == 'NUM' and flag == True:
+                flag = False
+            # convert tokens to base form
+            elif lemmatization == True and token.lemma_ != "-PRON-" and flag == True:
+                edit = token.lemma_
+            # append tokens edited and not removed to list
+            if edit != "" and flag == True:
+                clean_text.append(edit)
+        return " ".join(clean_text)

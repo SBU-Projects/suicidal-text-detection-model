7b6eeb64e4854533df2884d8b56bdf6f82435cca
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-02-10 12:03:50 +0330

    add frequency_bigramdictionary_en_243_342

--- main.py
+++ main.py
@@ -23,9 +23,7 @@ print(df)
 nlp = spacy.load("en_core_web_sm")
 vocab = collections.Counter()
 sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
-dictionary_path = pkg_resources.resource_filename(
-    "symspellpy", "Datasets/frequency_dictionary_en_82_765.txt")
-bigram_path = pkg_resources.resource_filename(
-"symspellpy", "frequency_bigramdictionary_en_243_342.txt")
+dictionary_path = "Datasets/frequency_dictionary_en_82_765.txt"
+bigram_path = "Datasets/frequency_bigramdictionary_en_243_342.txt"
 sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
 sym_spell.load_bigram_dictionary(bigram_path, term_index=0, count_index=2)

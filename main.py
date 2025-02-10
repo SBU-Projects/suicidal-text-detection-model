ecfd9fc49a0dc9c0805828b9c50fcc03508fb525
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-02-10 11:27:30 +0330

    install libs

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+import os
 import pandas as pd
+import numpy as np
 import spacy
+import unidecode
+import contractions as contract
+import re
+import wordninja
+import collections
+import pkg_resources
+from spellchecker import SpellChecker
+from symspellpy import SymSpell, Verbosity
 
 # Load dataset
 df = pd.read_csv('Datasets/Suicide_Ideation_Dataset(Twitter-based).csv')
 df.reset_index(drop=True, inplace=True)
-
+""".
 df['Suicide'] = df['Suicide'].replace({'Potential Suicide post ': 1, 'Not Suicide post': 0})
 print(df)
+."""
+# Defining methods
+
+nlp = spacy.load("en_core_web_sm")
+vocab = collections.Counter()
+sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
+dictionary_path = pkg_resources.resource_filename(
+    "symspellpy", "frequency_dictionary_en_82_765.txt")
+bigram_path = pkg_resources.resource_filename(
+"symspellpy", "frequency_bigramdictionary_en_243_342.txt")
+sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
+sym_spell.load_bigram_dictionary(bigram_path, term_index=0, count_index=2)

e211255a7f3ce4fc22af9506d04941305c96042f
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-02-10 11:21:20 +0330

    use spacy

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import spacy
 
 # Load dataset
 df = pd.read_csv('Datasets/Suicide_Ideation_Dataset(Twitter-based).csv')
 df.reset_index(drop=True, inplace=True)
 
-print(df.head())
+df['Suicide'] = df['Suicide'].replace({'Potential Suicide post ': 1, 'Not Suicide post': 0})
+print(df)

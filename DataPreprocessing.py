5544f6bc1495ece0a9650380bb6c985d9054b2ba
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-02-10 12:28:18 +0330

    dp libs

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -1,3 +1,16 @@
+import os
+import pandas as pd
+import numpy as np
+import spacy
+import unidecode
+import contractions as contract
+import re
+import wordninja
+import collections
+import pkg_resources
+from spellchecker import SpellChecker
+from symspellpy import SymSpell, Verbosity
+
 class DP:
     def __init__(self, dataframe):
         self.dataframe = dataframe

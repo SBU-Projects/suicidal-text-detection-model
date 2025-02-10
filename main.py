c7e4c4607935f49adb55d1fdc7dedf5ccdad9240
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-02-10 12:37:58 +0330

    test dp on main file

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import pkg_resources
 from spellchecker import SpellChecker
 from symspellpy import SymSpell, Verbosity
 
+from DataPreprocessing import DP
+
 # Load dataset
 df = pd.read_csv('Datasets/Suicide_Ideation_Dataset(Twitter-based).csv')
 df.reset_index(drop=True, inplace=True)
@@ -18,14 +20,5 @@ df.reset_index(drop=True, inplace=True)
 df['Suicide'] = df['Suicide'].replace({'Potential Suicide post ': 1, 'Not Suicide post': 0})
 print(df)
 ."""
-# Defining methods
-nlp = spacy.load("en_core_web_sm")
-vocab = collections.Counter()
-sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
-dictionary_path = "Datasets/frequency_dictionary_en_82_765.txt"
-bigram_path = "Datasets/frequency_bigramdictionary_en_243_342.txt"
-sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
-sym_spell.load_bigram_dictionary(bigram_path, term_index=0, count_index=2)
 
-
-
+dp = DP(df)

467b202e6965f80c26abb47da34985a0e5ec6b8d
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-02-11 17:33:34 +0330

    expand data libs similar sentences

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
-import random
+import nlpaug.augmenter.word as naw
 
-def random_insertion(sentence, n=1):
-    words = sentence.split()
-    for _ in range(n):
-        word = random.choice(words)
-        words.insert(random.randint(0, len(words)), word)
-    return ' '.join(words)
-
-def random_deletion(sentence, p=0.1):
-    words = sentence.split()
-    if len(words) == 1:
-        return sentence
-    remaining = list(filter(lambda x: random.random() > p, words))
-    if len(remaining) == 0:
-        return random.choice(words)
-    return ' '.join(remaining)
-
-# مثال استفاده
-sentence = "I want to kill myself"
-similar_sentence = random_insertion(sentence, n=2)
-print(similar_sentence)
-similar_sentence = random_deletion(sentence, p=0.2)
+# ایجاد جملات مشابه با استفاده از WordNet
+aug = naw.SynonymAug(aug_src='wordnet')
+sentence = "I love programming in Python"
+similar_sentence = aug.augment(sentence)
 print(similar_sentence)

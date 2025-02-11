713ac917d9b5ee24eda4cdfaff3f16f11603b326
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-02-11 17:58:37 +0330

    libs

--- main.py
+++ main.py
@@ -3,6 +3,42 @@ from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 from DataPreprocessing import DP
 
+
+import re
+import string
+
+from sklearn.base import TransformerMixin, BaseEstimator
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.calibration import CalibratedClassifierCV
+from imblearn.under_sampling import InstanceHardnessThreshold
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_selection import SelectFromModel
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.naive_bayes import MultinomialNB, ComplementNB
+from sklearn.svm import LinearSVC
+
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+from sklearn.feature_selection import SelectFromModel
+
+from imblearn.pipeline import Pipeline
+import pickle
+
+
+import spacy
+nlp = spacy.load("en_core_web_lg")
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
 df = pd.read_csv("Datasets/suicide_detection_final_cleaned.csv")
 dp = DP(df)
 df = df[~df['Tweet'].apply(lambda x: isinstance(x, float))]

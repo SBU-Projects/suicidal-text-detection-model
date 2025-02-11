9453d8efa60908a8b20057a7f943db9efef59816
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-02-11 17:31:36 +0330

    expand data libs similar sentences

--- main.py
+++ main.py
@@ -1,35 +1,24 @@
 import random
-from nltk.corpus import wordnet
 
-
-def get_synonyms(word):
-    synonyms = set()
-    for syn in wordnet.synsets(word):
-        for lemma in syn.lemmas():
-            synonyms.add(lemma.name())
-    return list(synonyms)
-
-
-def synonym_replacement(sentence, n=1):
+def random_insertion(sentence, n=1):
     words = sentence.split()
-    new_sentence = words.copy()
-    random_word_list = list(set([word for word in words if len(get_synonyms(word)) > 0]))
-    random.shuffle(random_word_list)
-    num_replaced = 0
+    for _ in range(n):
+        word = random.choice(words)
+        words.insert(random.randint(0, len(words)), word)
+    return ' '.join(words)
 
-    for random_word in random_word_list:
-        synonyms = get_synonyms(random_word)
-        if len(synonyms) > 0:
-            synonym = random.choice(synonyms)
-            new_sentence = [synonym if word == random_word else word for word in new_sentence]
-            num_replaced += 1
-        if num_replaced >= n:
-            break
-
-    return ' '.join(new_sentence)
-
+def random_deletion(sentence, p=0.1):
+    words = sentence.split()
+    if len(words) == 1:
+        return sentence
+    remaining = list(filter(lambda x: random.random() > p, words))
+    if len(remaining) == 0:
+        return random.choice(words)
+    return ' '.join(remaining)
 
 # مثال استفاده
 sentence = "I love programming in Python"
-similar_sentence = synonym_replacement(sentence, n=2)
+similar_sentence = random_insertion(sentence, n=2)
+print(similar_sentence)
+similar_sentence = random_deletion(sentence, p=0.2)
 print(similar_sentence)
